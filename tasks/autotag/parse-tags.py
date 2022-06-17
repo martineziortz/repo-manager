219b2be2425918ca10e69e172e30b17031798b68
Steven Wichers <me@stevenwichers.com>
2022-06-17 01:27:48 -0700

    Composer key name was incorrect

--- tasks/autotag/parse-tags.py
+++ tasks/autotag/parse-tags.py
@@ -31,7 +31,7 @@ for tag, needles in mapping_data.items():
             continue
 
         matches_repo = needle in args.get('repo_name')
-        matches_composer_name = needle in composer_data.get('names') or ''
+        matches_composer_name = needle in composer_data.get('name') or ''
         matches_composer_desc = needle in composer_data.get('description') or ''
 
         if (matches_repo or matches_composer_name or matches_composer_desc):
